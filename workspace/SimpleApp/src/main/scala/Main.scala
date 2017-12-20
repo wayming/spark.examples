3b3c3eaa09a7fee0f90064d372a32d46db5bc8bf
wayming.z <wayming.z@gmail.com>
2017-12-20 22:48:32 +1000

    Generate data

--- workspace/SimpleApp/src/main/scala/Main.scala
+++ workspace/SimpleApp/src/main/scala/Main.scala
@@ -1,8 +1,11 @@
-package main.scala
+import SimpleApp.UserData
 
 object Main {
 
   def main(args: Array[String]) {
     println("Hello World")
+    val dataGenerator = new UserData
+    dataGenerator.GenerateUsers("src/main/resources/users1.txt", 1000)
+    dataGenerator.GenerateFollowers("src/main/resources/followers1.txt", 1000)
   }
 }
---
+++ workspace/SimpleApp/src/main/scala/SimpleApp/UserData.scala
@@ -0,0 +1,28 @@
+package SimpleApp
+
+import java.io.{BufferedWriter, File, FileWriter, PrintWriter}
+
+import scala.io._
+import scala.util.Random
+
+class UserData {
+  def GenerateUsers(fileName: String, range: Int): Unit = {
+    val fileHandle = new BufferedWriter(new FileWriter(new File(fileName)))
+
+    val output = for (i <- 1 to range) yield i.toString + "," + RandomString(5 + Random.nextInt(20))
+    fileHandle.write(output.mkString("\n"))
+    fileHandle.close()
+  }
+
+  def RandomString(length: Int): String = {
+    Random.alphanumeric.take(length).mkString("")
+  }
+
+  def GenerateFollowers(fileName: String, range: Int): Unit = {
+    val fileHandle = new BufferedWriter(new FileWriter(new File(fileName)))
+    val output = for (i <- 1 to range * range)
+        yield (1+Random.nextInt(range)).toString + " " + (1+Random.nextInt(range)).toString
+    fileHandle.write(output.mkString("\n"))
+    fileHandle.close()
+  }
+}
