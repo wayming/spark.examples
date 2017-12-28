40829c43e7039e823fc99fc3e7bf895a445708f2
wayming.z <wayming.z@gmail.com>
2017-12-28 23:31:30 +1000

    use data frame and spark session api

--- workspace/SimpleApp/src/main/scala/SimpleApp/SVTrace.scala
+++ workspace/SimpleApp/src/main/scala/SimpleApp/SVTrace.scala
@@ -2,33 +2,35 @@ package SimpleApp
 
 import java.text.SimpleDateFormat
 
+import org.apache.spark.sql.{SQLContext, SparkSession}
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions._
 
 class SVTrace {
   def ConsumingTime(fileName: String, top: Int): Unit = {
-    val conf = new SparkConf()
-    conf.setAppName("ConsumingTime")
-    conf.setMaster("local")
 
-    val sc = new SparkContext(conf)
+    val ss = SparkSession.builder()
+                  .master("local")
+                  .appName("Trace time")
+                  .getOrCreate()
 
-    val traceFile = sc.textFile(fileName)
-
-    println(traceFile.count())
+    import ss.implicits._
 
     val dateFormat = new SimpleDateFormat("hh:mm:ss MMM dd")
-    val result =
-      traceFile.map((line) => {
-      val timeStamps = line.split(" ").take(3)
-      val microSeconds = timeStamps(0).split("\\.")(1)
-      timeStamps(0) = timeStamps(0).split("\\.")(0)
 
-      val epochSeconds =
-        dateFormat.parse(timeStamps.mkString(" ")).toInstant.getEpochSecond
-      val epochTime = epochSeconds.toDouble * 1000000 + microSeconds.toDouble
-      (epochTime, line)
-    })
+    val traceFile =
+      ss.read.textFile(fileName).map((line) => {
+        val timeStamps = line.split(" ").take(3)
+        val microSeconds = timeStamps(0).split("\\.")(1)
+        timeStamps(0) = timeStamps(0).split("\\.")(0)
 
-    println(result.toString())
+        val epochSeconds =
+          dateFormat.parse(timeStamps.mkString(" ")).toInstant.getEpochSecond
+        val epochTime = epochSeconds.toDouble * 1000000 + microSeconds.toDouble
+        (epochTime, line)
+      }).toDF("time", "operation")
+
+    println(traceFile.show())
   }
 }
