00b016c0442cb1b2b811f39bc2cb477e1069d75e
wayming.z <wayming.z@gmail.com>
2017-12-29 23:21:23 +1000

    artifacts

--- workspace/SimpleApp/src/main/scala/Main.scala
+++ workspace/SimpleApp/src/main/scala/Main.scala
@@ -1,11 +1,14 @@
-import SimpleApp.UserData
+package SimpleApp
 
 object Main {
 
   def main(args: Array[String]) {
     println("Hello World")
     val dataGenerator = new UserData
-    dataGenerator.GenerateUsers("src/main/resources/users1.txt", 100)
-    dataGenerator.GenerateFollowers("src/main/resources/followers1.txt", 100)
+    dataGenerator.GenerateUsers(args(0), 100)
+    dataGenerator.GenerateFollowers(args(1), 100)
+
+    //dataGenerator.GenerateUsers("src/main/resources/users1.txt", 100)
+    //dataGenerator.GenerateFollowers("src/main/resources/followers1.txt", 100)
   }
 }
