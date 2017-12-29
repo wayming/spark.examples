df8c2f4fe112ca40618bc753328f3a57c8388623
wayming <wayming.z@gmail.com>
2017-12-29 12:45:36 +1000

    SVTrace

--- workspace/SimpleApp/src/main/scala/SimpleApp/SVTrace.scala
+++ workspace/SimpleApp/src/main/scala/SimpleApp/SVTrace.scala
@@ -2,8 +2,7 @@ package SimpleApp
 
 import java.text.SimpleDateFormat
 
-import org.apache.spark.sql.{SQLContext, SparkSession}
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.{Row, SparkSession}
 import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions._
 
@@ -26,11 +25,25 @@ class SVTrace {
         timeStamps(0) = timeStamps(0).split("\\.")(0)
 
         val epochSeconds =
-          dateFormat.parse(timeStamps.mkString(" ")).toInstant.getEpochSecond
-        val epochTime = epochSeconds.toDouble * 1000000 + microSeconds.toDouble
+            dateFormat.parse(timeStamps.mkString(" ")).toInstant.getEpochSecond
+        val epochTime = epochSeconds.toDouble + microSeconds.toDouble / 1000000
         (epochTime, line)
       }).toDF("time", "operation")
 
-    println(traceFile.show())
+    val timeSpec = Window.orderBy("time")
+    val timeDiff = traceFile.withColumn("pretime", lag(traceFile("time"), 1, 0).over(timeSpec))
+    timeDiff.show()
+    timeDiff.map((row) => {
+        var diff : Long = 0
+        row match {
+          case Row(a: Double, b: String, 0) => diff = 0
+          case Row(a: Double, b: String, c: Double) => {
+            diff = ((a - c) * 1000000).toLong
+            println(a + "-" + c + " = " + diff)
+          }
+
+        }
+        (row.getAs[Double]("time"), row.getAs[String]("operation"), diff)
+    }).toDF("time", "operation", "diff(micro)").orderBy(desc("diff(micro)")).show(top)
   }
 }
--- workspace/SimpleApp/src/test/scala/SVTraceSpec.scala
+++ workspace/SimpleApp/src/test/scala/SVTraceSpec.scala
@@ -3,10 +3,10 @@ import org.scalatest._
 
 class SVTraceSpec extends FlatSpec {
   "SVTrace app " should "run" in {
-    val rank = new SVTrace
+    val trace = new SVTrace
 
-    rank.ConsumingTime("src/main/resources/trerate.trc", 10)
-
+    //trace.ConsumingTime("src/main/resources/trerate.trc", 10)
+    trace.ConsumingTime("/sda3/trerate6.80.trc", 10)
     println("PageRank App Test Passed")
   }
 
